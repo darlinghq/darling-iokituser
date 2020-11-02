b476633fdf554551a3d6748468154f184ae22045
Ariel Abreu <facekapow@outlook.com>
2020-11-01 23:41:33 -0500

    Add some header stubs for Apple80211

---
+++ apple80211/apple80211_var.h
@@ -0,0 +1,15 @@
+// created for Darling
+
+#ifndef _IOKIT_APPLE80211_APPLE80211_VAR_H
+#define _IOKIT_APPLE80211_APPLE80211_VAR_H
+
+// probably incomplete
+struct opaque_presence_indication {
+	int* ssth;
+	int SUI;
+};
+
+// not sure what this is
+#define MAX_SSTH_SIZE 0
+
+#endif // _IOKIT_APPLE80211_APPLE80211_VAR_H
